c7483b27e0ece86cab5014864d1e1da79f31a53e
Abid Hasan Abir <abid.7.tm@gmial.com>
2024-11-05 11:27:06 +0600

    added spinner

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,21 +1,28 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet, useNavigation } from "react-router-dom";
+import './navbar.css'
 
 const Navbar = () => {
+
+    const navigation = useNavigation();
+    console.log(navigation.state);
+    
     return (
         <div>
             <h1>Navber</h1>
             <nav className="flex gap-5 ">
-                <Link to='/home'>Home</Link>
-                <Link to='/about'>About Us</Link>
-                <Link to='/contact'>Contact</Link>
-                <Link to='/footer'>Footer</Link>
-                <Link to='/users'>Users</Link>
-                <Link to='/posts'>Posts</Link>
-                <Link to='/albums'>Albums</Link>
-                <Link to='/comments'>Comments</Link>
+                <NavLink to='/home'>Home</NavLink>
+                <NavLink to='/about'>About Us</NavLink>
+                <NavLink to='/contact'>Contact</NavLink>
+                <NavLink to='/footer'>Footer</NavLink>
+                <NavLink to='/users'>Users</NavLink>
+                <NavLink to='/posts'>Posts</NavLink>
+                <NavLink to='/albums'>Albums</NavLink>
+                <NavLink to='/comments'>Comments</NavLink>
             </nav>
+            {
+                navigation.state === 'loading' ?<span className="loading loading-bars loading-lg"></span> : <Outlet></Outlet>
+            }
 
-            <Outlet></Outlet>
         </div>
     );
 };
